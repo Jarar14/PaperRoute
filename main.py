e086a23e925271be959efb3d914b17de13cf232d
Jarar Bangash <jararbangash@gmail.com>
2018-10-20 14:55:12 -0500

    Big-title Article URL scraper

--- main.py
+++ main.py
@@ -9,28 +9,52 @@ def run():
     url = urlopen("https://www.aljazeera.com/news")
     # GRAB HTML TAGS
     bsObj = BeautifulSoup(url, features="html.parser")
+    
     bigtitle = bsObj.findAll("h2", {"class": "top-sec-title"})
-    smalltitle = bsObj.findAll("h2", {"class": "top-sec-smalltitle"})
+    bigLink = bsObj.find("div", {"class": "top-feature-overlay-cont"})
+
+    bigURL = []
+    possible_links = bigLink.find_all('a')
+    for url in possible_links:
+	    if url.has_attr('href'):
+		    bigURL.append(url.attrs['href'])
+        
+    print(bigURL[1]) #has link to big article
 
     # ITERATE OVER TAGS AND GRAB ARTICLE TITLES
-    i = 0
     for article in bigtitle:
         heading_data = article.text
         print(heading_data)
 
-        article_file.write(heading_data)
-        article_file.write('\n')
+        article_file.write(heading_data + " // ") #article title
 
-        i = i + 1
+        
+        article_file.write('\n') #newline before next entry
 
-    m = 0
+    print("\n")
+
+    smalltitle = bsObj.findAll("h2", {"class": "top-sec-smalltitle"})
+    
+    """
+
+
+    smallLink = bsObj.find("div", {"class": "topFeature-sblock-wr"})
+
+    smallURL = []
+    possible_links = smallLink.find_all('a')
+    i = 0
+    for url in possible_links:
+	    if url.has_attr('href'):
+		    smallURL.append(url.attrs['href'])
+		    print(smallURL[i])
+		    i +=1
+"""
+		    
     for article in smalltitle:
         heading_data = article.text
         print(heading_data)
 
         article_file.write(heading_data)
         article_file.write("\n")
-
-        m = m + 1
 
 run()
