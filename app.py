e85a1ed50d6b151b66bee1e3018d16671707ff11
Omer Khan <okhanlodhi@gmail.com>
2018-10-20 12:22:33 -0500

    Commit flask app

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+with open("day1.txt", 'r') as f:
+    content = f.read()
+
+@app.route('/')
+def index():
+    return render_template("index.html", content=content)
+
+if __name__ == '__main__':
+    app.run(debug=True)
